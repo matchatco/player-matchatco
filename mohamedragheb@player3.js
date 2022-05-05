a5b60d63823c68b2efee2d308b4368fe23aeada9
matchatco <102745466+matchatco@users.noreply.github.com>
2022-05-05 10:38:27 +0200

    Create mohamedragheb@player3.js

---
+++ mohamedragheb@player3.js
@@ -0,0 +1,6 @@
+var srcx = "https://admdn1.cdn.mangomolo.com/adsports1/smil:adsports1.stream.smil/playlist.m3u8";
+var namechannel = "إسم القناة";
+var user_id = "";
+var tokent = "5384568425:AAEg-af24bS0BWMkVosY0rebAycibgghU5U";
+var message1report = "هل القناة لا تعمل أو توجد بها مشكلة ؟؟";
+var message2report = "تم إرسال بلاغك و إذا كانت توجد مشكلة سيتم إصلاحها في أقرب وقت";
